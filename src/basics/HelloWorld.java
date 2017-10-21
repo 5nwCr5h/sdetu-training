2fd44b4957855b833dd9d31cfa4aa9733cfe6b14
5nwCr5h <32943563+5nwCr5h@users.noreply.github.com>
2017-10-21 14:35:45 +0000

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 		public static void main (String[] args){
 			System.out.println("Hello World");
 			System.out.println("I'm adding code to my project"); // code change to update GitHub
+			System.out.println("Adding code from GitHub ie modifying/updating HelloWorld.java file on GitHub");
 		}
 }
