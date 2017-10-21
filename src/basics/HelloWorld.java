ac649c0fe97cc50efad6af83771e6b72c23558ac
da43m0n <da43m0n@da43m0ns-MacBook-Pro.local>
2017-10-21 07:25:32 -0700

    Modified Hello World app

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 		// TODO Auto-generated constructor stub
 		public static void main (String[] args){
 			System.out.println("Hello World");
+			System.out.println("I'm adding code to my project"); // code change to update GitHub
 		}
 }
